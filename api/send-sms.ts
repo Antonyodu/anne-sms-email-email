46a90fdda7aa7d4a57debf77b514a966af45610c
Anthony Odu <antonyodu@gmail.com>
2023-11-17 10:29:06 +0100

    Updates

--- api/send-sms.ts
+++ api/send-sms.ts
@@ -3,43 +3,45 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const { subject, body } = req.body;
+  try {
+    const { subject, body } = req.body;
 
-  const accountSid = 'AC4ace975430e21483e572e120cb6a0b96';
-  const authToken = 'fb4eaed8dba66ded28169729fe90b215';
-  const twilioPhone = '+13344630152';
-  const recipientPhone = subject;
-  const messageBody = body;
+    if (!subject || !body) {
+      return res.status(400).json({ error: 'Invalid request body' });
+    }
 
-  const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
+    const accountSid = 'AC4ace975430e21483e572e120cb6a0b96';
+    const authToken = 'fb4eaed8dba66ded28169729fe90b215';
+    const twilioPhone = '+13344630152';
+    const recipientPhone = subject;
+    const messageBody = body;
 
-  const authHeader = 'Basic ' + Buffer.from(`${accountSid}:${authToken}`).toString('base64');
+    const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
 
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': authHeader,
-    },
-    body: new URLSearchParams({
-      'To': recipientPhone,
-      'From': twilioPhone,
-      'Body': messageBody,
-    } as string),
-  })
-    .then(response => response.json())
-    .then(data => {
-      // Return Twilio response to the client
-      return res.json({
-        data: {
-          subject,
-          body,
-          twilioResponse: data, // Include Twilio response in the JSON output
-        },
-      });
-    })
-    .catch(error => {
-      console.error('Error sending message:', error);
-      return res.status(500).json({ error: 'Error sending message' });
+    const authHeader = 'Basic ' + Buffer.from(`${accountSid}:${authToken}`).toString('base64');
+
+    const twilioResponse = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': authHeader,
+      },
+      body: new URLSearchParams({
+        'To': recipientPhone,
+        'From': twilioPhone,
+        'Body': messageBody,
+      } as any), // 'as any' if TypeScript complains
+    }).then(response => response.json());
+
+    return res.json({
+      data: {
+        subject,
+        body,
+        twilioResponse,
+      },
     });
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
