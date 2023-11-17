487e0b94ddc1145d0c12bf5c1e4aecdfeacbf444
Anthony Odu <antonyodu@gmail.com>
2023-11-17 10:31:12 +0100

    updates

--- api/send-sms.ts
+++ api/send-sms.ts
@@ -2,7 +2,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   try {
     const { subject, body } = req.body;
 
