c9802d0f7d4bbbafd40a52359c1ef2840ccdabae
Anthony Odu <antonyodu@gmail.com>
2023-11-17 10:35:19 +0100

    ...

--- api/send-sms.ts
+++ api/send-sms.ts
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
   try {
-    const { subject, body } = JSON.parse(req.query);
+    const { subject, body } = JSON.parse(req.body);
 
     if (!subject || !body) {
       return res.status(400).json({ error: 'Invalid request body' });
