af1a4c72ab98f0917bbdf2d3a343eaf8966895aa
Anthony Odu <antonyodu@gmail.com>
2023-11-17 10:44:10 +0100

    Updteas

--- api/send-sms.ts
+++ api/send-sms.ts
@@ -1,40 +1,44 @@
 // @ts-ignore
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch';
+import multer from 'multer';
+import util from 'util';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-    // Assuming the request body is in JSON format
-    const { subject, body } = (req.body);
+  await util.promisify(multer().any())(req, res);
+  // Assuming the request body is in JSON format
+  const subject = req.body.subject;
+  const body = req.body.body;
 
-    const accountSid = 'AC4ace975430e21483e572e120cb6a0b96';
-    const authToken = 'fb4eaed8dba66ded28169729fe90b215';
-    const twilioPhone = '+13344630152';
-    const recipientPhone = subject;
-    const messageBody = body;
+  const accountSid = 'AC4ace975430e21483e572e120cb6a0b96';
+  const authToken = 'fb4eaed8dba66ded28169729fe90b215';
+  const twilioPhone = '+13344630152';
+  const recipientPhone = subject;
+  const messageBody = body;
 
-    const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
+  const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
 
-    const authHeader = 'Basic ' + Buffer.from(`${accountSid}:${authToken}`).toString('base64');
+  const authHeader = 'Basic ' + Buffer.from(`${accountSid}:${authToken}`).toString('base64');
 
-    const twilioResponse = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': authHeader,
-      },
-      body: new URLSearchParams({
-        'To': recipientPhone,
-        'From': twilioPhone,
-        'Body': messageBody,
-      } as any), // 'as any' if TypeScript complains
-    }).then(response => response.json());
+  const twilioResponse = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': authHeader,
+    },
+    body: new URLSearchParams({
+      'To': recipientPhone,
+      'From': twilioPhone,
+      'Body': messageBody,
+    } as any), // 'as any' if TypeScript complains
+  }).then(response => response.json());
 
-    return res.json({
-      data: {
-        subject,
-        body,
-        twilioResponse,
-      },
-    });
-  
+  return res.json({
+    data: {
+      subject,
+      body,
+      twilioResponse,
+    },
+  });
+
 }
