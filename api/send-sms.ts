a00c09a6ff8dba605c87f9513036d0aec4e81e10
Anthony Odu <antonyodu@gmail.com>
2023-11-17 10:14:27 +0100

    Updates

--- api/send-sms.ts
+++ api/send-sms.ts
@@ -1,9 +1,9 @@
 // @ts-ignore
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch';
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { subject, body } = req.query
 
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const { subject, body } = req.query;
 
   const accountSid = 'AC4ace975430e21483e572e120cb6a0b96';
   const authToken = 'fb4eaed8dba66ded28169729fe90b215';
@@ -15,7 +15,7 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
 
   const authHeader = 'Basic ' + Buffer.from(`${accountSid}:${authToken}`).toString('base64');
 
-   fetch(url, {
+  fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
@@ -28,11 +28,18 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
     } as string),
   })
     .then(response => response.json())
-    .then(data => console.log('Message sent:', data))
-    .catch(error => console.error('Error sending message:', error));
-
-
-  return res.json({
-    data: { subject, body }
-  })
+    .then(data => {
+      // Return Twilio response to the client
+      return res.json({
+        data: {
+          subject,
+          body,
+          twilioResponse: data, // Include Twilio response in the JSON output
+        },
+      });
+    })
+    .catch(error => {
+      console.error('Error sending message:', error);
+      return res.status(500).json({ error: 'Error sending message' });
+    });
 }
