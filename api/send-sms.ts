d3d005ade1dda75fddc3520add0d2799b4b14793
Anthony Odu <antonyodu@gmail.com>
2023-11-17 11:13:50 +0100

    Updates

--- api/send-sms.ts
+++ api/send-sms.ts
@@ -1,20 +1,21 @@
 // @ts-ignore
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch';
-import multer from 'multer';
+// import multer from 'multer';
 import util from 'util';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  await util.promisify(multer().any())(req, res);
   // Assuming the request body is in JSON format
-  const subject = req.body.subject;
-  const body = req.body.body;
+  const emailStorage: Record<string, { subject: string, body: string }> = {};
 
   const accountSid = 'AC4ace975430e21483e572e120cb6a0b96';
   const authToken = 'fb4eaed8dba66ded28169729fe90b215';
   const twilioPhone = '+13344630152';
+  const payload = req.body;
+  const emailId = payload.sg_message_id; // Replace with the actual identifier in your emails
+  const emailContent = emailStorage[emailId];
+  const { subject, body } = emailContent;
   const recipientPhone = subject.split('|')[0].trim();
-  const messageBody = body;
 
   const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
 
@@ -27,9 +28,9 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       'Authorization': authHeader,
     },
     body: new URLSearchParams({
-      'To':  recipientPhone,
+      'To': recipientPhone,
       'From': twilioPhone,
-      'Body': messageBody,
+      'Body': body,
     } as any), // 'as any' if TypeScript complains
   }).then(response => response.json());
 
